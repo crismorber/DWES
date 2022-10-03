b6cdd40382e56e2283d67ad6b889a39518b56a97
Cristina Moreno <cmorenob@fpcoruna.afundacion.org>
2022-10-03 13:08:47 +0200

    Tarea 4 de PHP completada

---
+++ sprint1apache/www/site3/example4.php
@@ -0,0 +1,32 @@
+<html>
+	<body>
+		<h1>Jubilación</h1>
+		<?php
+			function edad_en_5_años($edad) {
+				return $edad + 5;
+			}
+			function mensaje($edad){
+				if (edad_en_5_años($edad) > 65) {
+					return "En 5 años tendrás edad de jubilación";
+				} else {
+					return "!Disfruta de tu tiempo!";
+				}
+			}
+		?>
+		<table>
+			<tr>
+				<th>Edad</th>
+				<th>Info</th>
+			</tr>
+			<?php
+				$lista = array (58,59,60,61,62);
+				foreach ($lista as $valor){
+					echo "<tr>";
+					echo "<td>".$valor."</td>";
+					echo "<td>".mensaje($valor)."</td>";
+					echo "</tr>";
+				}
+			?>
+		</table>
+	</body>
+</html>
