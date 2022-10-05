7d6a5bb182730323d5ee21630381ccedb7d5ce31
Cristina Moreno <cmorenob@fpcoruna.afundacion.org>
2022-10-05 14:20:30 +0200

    Ejercicio 1 terminado

---
+++ sprint2phpsql/www/mysite/main.php
@@ -0,0 +1,8 @@
+<html>
+	<body>
+		<?php
+			$db = mysqli_connect('localhost', 'root','1234','mysitedb') or die('Fail'. mysqli_connect_error());
+		?>
+		<h1>Conexión establecida</h1>
+	</body>
+</html>
