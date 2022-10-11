9326eb8e4df0007f8101d4fc72bc938e84e0e1f7
Cristina Moreno <cmorenob@fpcoruna.afundacion.org>
2022-10-11 13:34:11 +0200

    Tarea 2 completada

--- sprint2phpsql/www/mysite/main.php
+++ sprint2phpsql/www/mysite/main.php
@@ -1,8 +1,28 @@
+<?php
+	$db = mysqli_connect('localhost', 'root','1234','mysitedb') or die('Fail'. mysqli_connect_error());
+?>
 <html>
 	<body>
-		<?php
-			$db = mysqli_connect('localhost', 'root','1234','mysitedb') or die('Fail'. mysqli_connect_error());
-		?>
 		<h1>Conexión establecida</h1>
+		<?php 
+			//Lanzar una query
+			$query = 'SELECT * FROM tPeliculas';
+			
+			$result = mysqli_query($db, $query) or die('Query error');
+			//Recorrer el resultado
+			while ($row = mysqli_fetch_array($result)){
+			echo $row[0];
+			echo '<br>';
+			echo  $row['1'];
+			echo '<br>';
+			echo '<img src="'.$row['2'];
+			echo '">';
+			echo '<br>';
+			echo $row['3'];
+			echo '<br>';
+			echo $row['4'];
+			echo '<br>';
+			}
+		?>
 	</body>
 </html>
