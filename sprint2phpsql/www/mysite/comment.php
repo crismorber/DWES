5b8614c08f6de73aacafea34f65addb2adc2dfd8
Cristina Moreno <cmorenob@fpcoruna.afundacion.org>
2022-10-13 12:06:49 +0200

    Tarea 6 completada

--- sprint2phpsql/www/mysite/comment.php
+++ sprint2phpsql/www/mysite/comment.php
@@ -6,9 +6,11 @@
 	<?php
 		$pelicula_id  = $_POST['pelicula_id'];
 		$comentario = $_POST['new_comment'];
+		$fecha = date('Y-m-d');
 
-		$query = "INSERT INTO tComentarios (comentarios, usuario_id, pelicula_id) VALUES ('".$comentario."', NULL, ".$pelicula_id.");";
-		mysqli_query($db, $query) or die('Error');
+		$query = "INSERT INTO tComentarios (comentarios, usuario_id, pelicula_id, fecha) VALUES ('".$comentario."', NULL, ".$pelicula_id.",'".$fecha."');";
+echo $query;
+	 	mysqli_query($db, $query) or die('Error');
 
 		echo "<p> Nuevo comentario ";
 		echo mysqli_insert_id($db);
--- sprint2phpsql/www/mysite/detail.php
+++ sprint2phpsql/www/mysite/detail.php
@@ -23,7 +23,12 @@
 				$query2 = 'SELECT * FROM tComentarios WHERE pelicula_id='.$pelicula_id;
 				$result2 = mysqli_query($db, $query2) or die('Query error2');
 				while ($row = mysqli_fetch_array($result2)){
-					echo '<li>'.$row['1'].'</li>';
+					$fecha = $row['fecha'];
+					if (is_null($fecha)) {
+						echo '<li>'.$row['1'].' (Sin fecha registrada)</li>';
+					}else {
+						echo '<li>'.$row['1'].' ('.$row['fecha'].')</li>';
+					}
 				}
 				mysqli_close($db);
 			?>
