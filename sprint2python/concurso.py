b806de48c7ea01be91a8eab9aecec9e1a7c99877
Cristina Moreno <cmorenob@fpcoruna.afundacion.org>
2022-10-17 12:45:31 +0200

    Subo archivo python

---
+++ sprint2python/concurso.py
@@ -0,0 +1,20 @@
+print("¡Adivina la respuesta correcta!")
+
+print("En que año tuvo lugar la pandemia del coronavirus")
+print("a) 2018")
+print("b) 2020")
+print("c) 2022")
+
+respuesta = None;
+
+respuesta = input("Introduce una de las opciones (a, b, c): ")
+
+if (respuesta == "a" or respuesta=="c"):
+	print("Has fallado :(")
+if else respuesta =="b":
+	print ("Has acertado :)")
+else:
+	print ("Introduce una de las opciones (a, b, c): ")
+
+print("Gracias por jugar <3")
+
