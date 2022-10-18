51b09c14d42415c0ac0c64780fb118d75f8901c2
crismorber <113433738+crismorber@users.noreply.github.com>
2022-10-18 12:32:21 +0200

    Ejercicio 2 terminado

--- sprint2python/concurso.py
+++ sprint2python/concurso.py
@@ -1,21 +1,54 @@
 print("¡Adivina la respuesta correcta!")
 
-print("En que año tuvo lugar la pandemia del coronavirus")
-print("a) 2018")
-print("b) 2020")
-print("c) 2022")
+puntuacion=0
 
-respuesta = None
+preguntas = ["¿En qué año tuvo lugar la pandemia del coronavirus?","¿Cuántos colores tiene el arcoiris?","¿Qué idioma se habla en Moldavia?"]
 
-respuesta = input("Introduce una de las opciones (a, b, c): ")
+for i in range(3): 
+    print(preguntas[i])
+    respuesta=None
 
-while (respuesta !="a" and respuesta !="b" and respuesta!="c"):
-	respuesta = input ("Introduce una de las opciones (a, b, c): ")
+    if i==0:
+        print("a) 2022")
+        print("b) 2019")
+        print("c) 2020")
+        respuesta = respuesta = input ("Introduce una de las opciones (a, b, c): ")
+        while (respuesta !="a" and respuesta !="b" and respuesta!="c"):
+            respuesta = respuesta = input ("Introduce una de las opciones (a, b, c): ")
+        if (respuesta=="b" or respuesta =="a"):
+            print("Respuesta incorrecta :( Pierdes 5 puntos")
+            puntuacion = puntuacion - 5
+        elif (respuesta == "c"):
+            print("Respuesta correcta :) Ganas 10 puntos")
+            puntuacion = puntuacion + 10
+    if i==1:
+        print("a) 7")
+        print("b) 9")
+        print("c) 8")
+        respuesta = respuesta = input ("Introduce una de las opciones (a, b, c): ")
+        while (respuesta !="a" and respuesta !="b" and respuesta!="c"):
+            respuesta = respuesta = input ("Introduce una de las opciones (a, b, c): ")
+        if (respuesta=="c" or respuesta =="b"):
+            print("Respuesta incorrecta :( Pierdes 5 puntos")
+            puntuacion = puntuacion - 5
+        elif (respuesta == "a"):
+            print("Respuesta correcta :) Ganas 10 puntos")
+            puntuacion = puntuacion + 10
+    if i==2:
+        print("a) Ruso")
+        print("b) Rumano")
+        print("c) Inglés")
+        respuesta = respuesta = input ("Introduce una de las opciones (a, b, c): ")
+        while (respuesta !="a" and respuesta !="b" and respuesta!="c"):
+            respuesta = respuesta = input ("Introduce una de las opciones (a, b, c): ")
+        if (respuesta=="a" or respuesta =="c"):
+            print("Respuesta incorrecta :( Pierdes 5 puntos")
+            puntuacion = puntuacion - 5
+        elif (respuesta == "b"):
+            print("Respuesta correcta :) Ganas 10 puntos")
+            puntuacion = puntuacion + 10
 
-if (respuesta == "a" or respuesta=="c"):
-	print("Has fallado :(")
-elif  respuesta =="b":
-	print ("Has acertado :)")
 
-
-print("Gracias por jugar <3")
+print("Gracias por jugar <3 tu puntuación es de ", puntuacion, " puntos" )
+if puntuacion==30:
+    print("Felicidades, las acertaste todas!")
