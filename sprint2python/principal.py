db63c3e3399f505df5c8bd9aff4eec2e08a32adc
crismorber <113433738+crismorber@users.noreply.github.com>
2022-10-18 13:32:05 +0200

    Ejercicio 5 completado

--- sprint2python/principal.py
+++ sprint2python/principal.py
@@ -1,14 +1,20 @@
 from factorial import func_factorial
 from factorial2 import func_factorial2
 
-numero=input("Introduce un número para calcular su factorial recursivamente:")
+print ("a) Factorial de un número recursivamente")
+print ("b) Factorial de un número no recursivamente")
+respuesta = input("Elige una de las dos opciones (a o b): ")
 
-resultado = func_factorial(int(numero))
+while (respuesta!="a" and respuesta!="b"):
+    respuesta = input("Elige una de las dos opciones (a o b): ")
 
-print (resultado)
+if respuesta == "a":
+    numero=input("Introduce un número para calcular su factorial recursivamente: ")
+    resultado = func_factorial(int(numero))
+    print ("El factorial de",numero, "es", resultado)
+elif respuesta == "b":
+    numero=input("Introduce un número para calcular su factorial no recursivamente: ")
+    resultado = func_factorial2(int(numero))
+    print ("El factorial de",numero, "es", resultado)
 
-numero2=input("Introduce un número para calcular su factorial no recursivamente:")
-
-resultado2 = func_factorial2(int(numero2))
-
-print (resultado2)
+    
