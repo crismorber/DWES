0e81510341826b3167ae78d8180fac2382b1d490
crismorber <113433738+crismorber@users.noreply.github.com>
2022-10-19 14:02:08 +0200

    Tarea 7 corregida

--- sprint2python/concurso.py
+++ sprint2python/concurso.py
@@ -20,9 +20,9 @@ while veces < 3 :
         print("a) 2022")
         print("b) 2019")
         print("c) 2020")
-        respuesta = respuesta = input ("Introduce una de las opciones (a, b, c): ")
+        respuesta = input ("Introduce una de las opciones (a, b, c): ")
         while (respuesta !="a" and respuesta !="b" and respuesta!="c"):
-            respuesta = respuesta = input ("Introduce una de las opciones (a, b, c): ")
+            respuesta = input ("Introduce una de las opciones (a, b, c): ")
         if (respuesta=="b" or respuesta =="a"):
             print("Respuesta incorrecta :( Pierdes 5 puntos")
             puntuacion = puntuacion - 5
@@ -38,9 +38,9 @@ while veces < 3 :
         print("a) 7")
         print("b) 9")
         print("c) 8")
-        respuesta = respuesta = input ("Introduce una de las opciones (a, b, c): ")
+        respuesta = input ("Introduce una de las opciones (a, b, c): ")
         while (respuesta !="a" and respuesta !="b" and respuesta!="c"):
-            respuesta = respuesta = input ("Introduce una de las opciones (a, b, c): ")
+            respuesta = input ("Introduce una de las opciones (a, b, c): ")
         if (respuesta=="c" or respuesta =="b"):
             print("Respuesta incorrecta :( Pierdes 5 puntos")
             puntuacion = puntuacion - 5
@@ -57,9 +57,9 @@ while veces < 3 :
         print("a) Ruso")
         print("b) Rumano")
         print("c) Inglés")
-        respuesta = respuesta = input ("Introduce una de las opciones (a, b, c): ")
+        respuesta = input ("Introduce una de las opciones (a, b, c): ")
         while (respuesta !="a" and respuesta !="b" and respuesta!="c"):
-            respuesta = respuesta = input ("Introduce una de las opciones (a, b, c): ")
+            respuesta = input ("Introduce una de las opciones (a, b, c): ")
         if (respuesta=="a" or respuesta =="c"):
             print("Respuesta incorrecta :( Pierdes 5 puntos")
             puntuacion = puntuacion - 5
