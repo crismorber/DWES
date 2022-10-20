c99d1aa72207191d41670a3e1ee317fc381ed9b0
crismorber <113433738+crismorber@users.noreply.github.com>
2022-10-20 10:56:24 +0200

    Add files via upload

---
+++ sprint3sesiones/www/mysite/login.php
@@ -0,0 +1,20 @@
+<?php 
+    $db = mysqli_connect('localhost', 'root','1234','mysqli') or die('Fail');
+
+    $email_poster = $_POST['f_email'];
+    $password_posted = $_POST['f_password'];
+
+    $query = 'SELECT id, contraseña FROM tUsuarios where email = "'.$email_posted.'"';
+    $result = mysqli_query($db, $query) or die('Query error');
+    if (mysqli_num_rows($result)>0){
+        $only_row = mysqli_fetch_array($result);
+        if ($only_row[1] == $password_posted){
+            session_start();
+            $_SESSION['user_id']=$only_row[0];
+            header('Localhost: main.php');
+        } else{
+            echo '<p>Contraseña incorrecta</p>';
+        }
+    }else{
+        echo '<p>Usuario no encontrado con ese email</p>'
+    }
