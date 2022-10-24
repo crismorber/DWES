51debe75ca4d92c45a84a27fcbf4f8f3c4afccab
crismorber <113433738+crismorber@users.noreply.github.com>
2022-10-24 13:12:30 +0200

    Update main.php

--- sprint3sesiones/www/mysite/main.php
+++ sprint3sesiones/www/mysite/main.php
@@ -7,31 +7,56 @@
 			img{
 				height: 344px;
 				width: 260px;
+				transition: width 0.8s linear 0.2s;
+				transition: height 0.8s linear 0.2s;
+			}
+			img:hover{
+				height: 444pX;
+				width: 360px;
+			}
+			th{
+				color: blue;
+				transition: color 1s linear 0.3s;
+			}
+			th:hover{
+				color:red;
+			}
+			td:hover{
+				background-color: pink;
+			}
+			td{
+				transition: background-color 0.3s linear 0.5s;
 			}
 		</style>
 	</head>
 	<body>
 		<h1>Conexión establecida</h1>
-		<?php 
-			//Lanzar una query
-			$query = 'SELECT * FROM tPeliculas';
-			
-			$result = mysqli_query($db, $query) or die('Query error');
-			//Recorrer el resultado
-			while ($row = mysqli_fetch_array($result)){
-			echo $row[0];
-			echo '<br>';
-			echo  $row['1'];
-			echo '<br>';
-			echo '<img src="'.$row['2'];
-			echo '">';
-			echo '<br>';
-			echo $row['3'];
-			echo '<br>';
-			echo $row['4'];
-			echo '<br>';
-			}
-		?>
+		<table>
+			<tr>
+				<th>ID</th>
+				<th>Título</th>
+				<th>Foto</th>
+				<th>Valoración</th>
+				<td>Género</td>
+			</tr>
+			<?php 
+				//Lanzar una query
+				$query = 'SELECT * FROM tPeliculas';
+				
+				$result = mysqli_query($db, $query) or die('Query error');
+				//Recorrer el resultado
+				while ($row = mysqli_fetch_array($result)){
+					echo '<tr>';
+					echo '<td>'.$row[0].'</td>';
+					echo '<td>'.$row[1].'</td>';
+					echo '<td><img src="'.$row[2];
+					echo '"></td>';
+					echo '<td>'.$row[3].'</td>';
+					echo '<td>'.$row[4].'</td>';
+					echo '</tr>'
+				}
+			?>
+		</table>
         <a href="/logout.php">Logout</a>
 	</body>
 </html>
