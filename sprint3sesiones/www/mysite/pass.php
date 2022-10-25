2ee66ccd4f02dc44a6ea9652a048bbc9c24fe6a0
Cristina Moreno <cmorenob@fpcoruna.afundacion.org>
2022-10-25 13:49:28 +0200

    Tarea 8 completada php

--- sprint3sesiones/www/mysite/pass.php
+++ sprint3sesiones/www/mysite/pass.php
@@ -10,13 +10,13 @@
     $passwordA_posted = $_POST['f_passwordA'];
     $passwordN_posted = $_POST['f_passwordN'];
 
-    $query = 'SELECT id, contraseña FROM tUsuarios where id = "'.$id.'"';
+    $query = 'SELECT id, contraseña FROM tUsuarios where id = "'.$user.'"';
     $result = mysqli_query($db, $query) or die('Query error');
     if (mysqli_num_rows($result)>0){
         $only_row = mysqli_fetch_array($result);
-        if ($only_row[4] == $passwordA_posted){
-            $query = "UPDATE  tUsuarios SET contraseña = '".$passwordN_posted."' WHERE id =".$id.'; ';
-            $result = mysqli_query($db, $query2) or die('Query error');
+        if ($only_row[1] == $passwordA_posted){
+            $query2 = "UPDATE  tUsuarios SET contraseña = '".$passwordN_posted."' WHERE id =".$user.'; ';
+            $result = mysqli_query($db, $query2) or die('Query2 error');
             header('Location: main.php');
         } else{
             echo '<p>Contraseña incorrecta</p>';
