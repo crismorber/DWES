46e972313a7e3a84628472a908506361cf897d3b
crismorber <113433738+crismorber@users.noreply.github.com>
2022-10-25 13:30:22 +0200

    Update pass.php

--- sprint3sesiones/www/mysite/pass.php
+++ sprint3sesiones/www/mysite/pass.php
@@ -1,9 +1,14 @@
 <?php 
     $db = mysqli_connect('localhost', 'root','1234','mysitedb') or die('Fail');
 
+
+    session_start();
+    $user = 'NULL';
+    if (!empty($_SESSION['user_id'])) {
+        $user = $_SESSION['user_id'];
+    }
     $passwordA_posted = $_POST['f_passwordA'];
     $passwordN_posted = $_POST['f_passwordN'];
-    $id = $_SESSION['user_id'];
 
     $query = 'SELECT id, contraseña FROM tUsuarios where id = "'.$id.'"';
     $result = mysqli_query($db, $query) or die('Query error');
@@ -11,8 +16,10 @@
         $only_row = mysqli_fetch_array($result);
         if ($only_row[4] == $passwordA_posted){
             $query = 'UPDATE  tUsuarios SET contraseña = "'.$passwordN_posted.'" WHERE id ='.$id.'; ';
+            $result = mysqli_query($db, $query2) or die('Query error');
             header('Location: main.php');
         } else{
             echo '<p>Contraseña incorrecta</p>';
         }
     }
+    ?>
