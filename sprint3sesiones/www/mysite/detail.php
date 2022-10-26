a53af0a5029289b4b575d3c729c823580529b63c
crismorber <113433738+crismorber@users.noreply.github.com>
2022-10-26 13:47:44 +0200

    Update detail.php

--- sprint3sesiones/www/mysite/detail.php
+++ sprint3sesiones/www/mysite/detail.php
@@ -21,13 +21,15 @@
 		<ul>
 			<?php
 				$query2 = 'SELECT * FROM tComentarios WHERE pelicula_id='.$pelicula_id;
+				$query3 = 'SELECT tUsuarios.nombre FROM tUsuarios, tComentarios WHERE tUsuarios.id=usuario_id and pelicula_id='.$pelicula_id;
 				$result2 = mysqli_query($db, $query2) or die('Query error2');
-				while ($row = mysqli_fetch_array($result2)){
+				$result3 = mysqli_query($db, $query3) or die('Query error3');
+				while ($row = mysqli_fetch_array($result2) and $row2=mysqli_fetch_array($result3)) {
 					$fecha = $row['fecha'];
 					if (is_null($fecha)) {
-						echo '<li>'.$row['1'].' (Sin fecha registrada)</li>';
+						echo '<li>'.$row['comentario']." ".$row2['nombre']." (Sin fecha registrada) </li>";
 					}else {
-						echo '<li>'.$row['1'].' ('.$row['fecha'].')</li>';
+						echo '<li>'.$row['comentario'].'  '.$row2['nombre']." ".$row['fecha']."</li>";
 					}
 				}
 				mysqli_close($db);
