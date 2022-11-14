b7fc4271434b4a9a8a47be09191190b5215538c6
Cristina Moreno <cmorenob@fpcoruna.afundacion.org>
2022-11-14 13:29:37 +0100

    Corregidos archivos urls.py y localhost funcionando

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,9 +1,7 @@
 from . import views
 
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 
 urlpatterns = [
-	path('polls/', include('polls.urls')),
-	path('admin/', admin.site.urls),
+	path('', views.index, name='index'),
 ]
